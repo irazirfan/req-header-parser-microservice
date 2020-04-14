956832c156fb026510c99b38a973e85e727bcb1c
Glitch (past-honorable-scabiosa) <none>
2020-04-14 12:13:57 +0000

    ✈️🚘 Updated with Glitch

--- api/request.js
+++ api/request.js
@@ -1,31 +1,27 @@
-'use strict';
+"use strict";
 
-module.exports = function(app){
-  
-  var accepts = require('accepts');
+module.exports = function(app) {
+  var accepts = require("accepts");
   //load accepts module to parse accept-language string req http header
   //.languages methods returns an array in the order of clients preferences
-  
-  var uaParser = require('ua-parser');
+
+  var uaParser = require("ua-parser");
   //load ua-parser module that uses regex library to parse user agent strings in req http header
   //Added app.enable('trust proxy') to server.js app intialization code to maket this work
-    
-  app.get('/', function(req,res){
-             
+
+  app.get("/", function(req, res) {
     var ip = req.ip;
     //get ip address with express build-in method req.ip
-    
+
     var language = accepts(req).languages()[0];
     //get an array of preferred languages from http req headers, and take the first one, which is most preferred language
-        
-    var uaHeader = req.headers['user-agent'];
-    //get user-agent string 
-    
+
+    var uaHeader = req.headers["user-agent"];
+    //get user-agent string
+
     var agent = uaParser.parseOS(uaHeader).toString();
     //parse OS part of user-agent string to a string
-     
-     res.json({"ipaddress": ip, "language": language, "software": agent});
-      
+
+    res.json({ ipaddress: ip, language: language, software: agent });
   });
-    
 };
